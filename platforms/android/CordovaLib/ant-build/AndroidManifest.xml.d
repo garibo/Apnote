3748dbbea3c979c6e880ee9f41d9ba9e1ade8931
coderdiaz <javier.diaz.internaut@gmail.com>
2014-09-09 01:16:15 -0500

    Recuperado

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\www\data\localweb\Apnote\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\www\data\localweb\Apnote\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\www\data\localweb\Apnote\platforms\android\ant-build\AndroidManifest.xml : \
+C:\www\data\localweb\Apnote\platforms\android\AndroidManifest.xml \
